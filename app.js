ec1b5d9d41466b7ea5e721a1fd66c79f097842bc
Hardik Gupta <cu.18bcs3199@gmail.com>
2023-01-24 16:16:40 -0600

    commit in the class

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ app.get("/", function (req, res) {
    //res.sendFile(__dirname + "/example");
     res.send("Hello fr express");
 });
+
+app.get('/show', (req, res)=> {
+    
+    res.sendFile('index.html', {root: __dirname});
+
+})
 app.listen(3000, function () {
     console.log("Server is running on localhost3000");
 });
