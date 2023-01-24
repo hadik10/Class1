82e1b8b6258a3fe16bc89d717a3ced068e553405
Hardik Gupta <cu.18bcs3199@gmail.com>
2023-01-24 16:41:24 -0600

    commit ejs

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 
+// set the view engine to ejs
+app.set('view engine','ejs');
+
+let myName = 'Hardik';
+
 app.get("/", function (req, res) {
    //res.sendFile(__dirname + "/example");
-    res.send("Hello fr express");
+    res.send(`<h3>Hi, ${myName} </h3>`);
 });
 
 app.get('/show', (req, res)=> {
@@ -11,6 +16,15 @@ app.get('/show', (req, res)=> {
     res.sendFile('index.html', {root: __dirname});
 
 })
+
+app.get('ejs', (req, res)=> {
+
+    //ejs stuff goes here.
+    
+    //use res.render to load up an ejs view file
+    res.render('index');
+})
+
 app.listen(3000, function () {
     console.log("Server is running on localhost3000");
 });
